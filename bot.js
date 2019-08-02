4568eb647d6b2802ffc1e0fcd74436f20fc3a600
AnonimYYYs <53582503+AnonimYYYs@users.noreply.github.com>
2019-08-02 16:35:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+
+client.login('NTk5MjE1NjYzMjk4Mzc5Nzg2.XSh9jA.AmBwg4rbK294Ji6i1JtBcLZVfxA');
 
 client.on('message', message => {
     if (message.content === 'ping') {
@@ -11,5 +10,3 @@ client.on('message', message => {
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
