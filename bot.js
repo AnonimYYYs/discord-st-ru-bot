6f4b0a71994c47d5d3c66c298f4a0ce2c6231bf4
AnonimYYYs <53582503+AnonimYYYs@users.noreply.github.com>
2019-08-02 16:59:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,9 @@ const client = new Discord.Client();
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+  	} else
+    if (message.content === 'Кто лучший игрок?') {
+    	message.reply('Бот лучший игрок!');
   	}
 });
 
