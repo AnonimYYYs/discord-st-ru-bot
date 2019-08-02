06c7b88bdf270d5cafa2d824ad3059c7358bc9a1
AnonimYYYs <53582503+AnonimYYYs@users.noreply.github.com>
2019-08-02 16:43:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.login('NTk5MjE1NjYzMjk4Mzc5Nzg2.XUQ5-A.AKE9zIRRuWLo1NJVx9hKrZvc3Es');
-
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
