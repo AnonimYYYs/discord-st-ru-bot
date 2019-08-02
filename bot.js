d1f83c2083209a26696436199cb8b5fee6fe7a06
AnonimYYYs <53582503+AnonimYYYs@users.noreply.github.com>
2019-08-02 16:38:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.login('NTk5MjE1NjYzMjk4Mzc5Nzg2.XSh9jA.AmBwg4rbK294Ji6i1JtBcLZVfxA');
+client.login('NTk5MjE1NjYzMjk4Mzc5Nzg2.XUQ5-A.AKE9zIRRuWLo1NJVx9hKrZvc3Es');
 
 client.on('message', message => {
     if (message.content === 'ping') {
